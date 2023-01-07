61973db897e9b68ffc5cf8722e4e87a9e45e5e47
nema <zpredrag@gmail.com>
2023-01-06 23:30:18 -0500

    add files

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-git add *
-git commit -m "add files"
-git push https://lapovogit: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@github.com/lapovogit/repo-test.git/
+git -C /home/nema/repo/repo-test/ add *
+git -C /home/nema/repo/repo-test/ commit -m "add files"
+git -C /home/nema/repo/repo-test/ push https://lapovogit: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@github.com/lapovogit/repo-test.git/
 
